313984285753182c6a16483490fd37a3f9f1c3d8
doubleface <christophe@cozycloud.cc>
2018-10-04 22:01:13 +0200

    feat: now the list of connectors comes from the registry

--- src/helpers/list.js
+++ src/helpers/list.js
@@ -1,21 +1,27 @@
-import repos from '../../list.json'
 import { includes } from 'lodash'
+import request from 'request-promise'
+import gitParser from 'git-url-parse'
 
-const REPO_REGEXP = /^(git\+)?https?:\/\/([a-zA-Z0-9-_.]+)\/([a-zA-Z0-9-_.]+)\/([a-zA-Z0-9-_.]+)$/
 const PROVIDER_GITHUB = 'github.com'
 const PROVIDER_GITLAB = 'gitlab.cozycloud.cc'
 
 export const parsingRepository = url => {
-  const matching = url.match(REPO_REGEXP)
+  const parsed = gitParser(url)
   return {
-    provider: matching[2] === PROVIDER_GITHUB ? 'Github' : 'Gitlab',
-    owner: matching[3],
-    repoName: matching[4],
+    provider: parsed.resource === PROVIDER_GITHUB ? 'Github' : 'Gitlab',
+    owner: parsed.owner,
+    repoName: parsed.name,
     url
   }
 }
 
-export const getRepositories = (provider = '') => {
+export const getRepositories = async (provider = '') => {
+  const repos = (await request.get(
+    'https://apps-registry.cozycloud.cc/registry?limit=10000',
+    { json: true }
+  )).data
+    .filter(repo => repo.latest_version && repo.type === 'konnector')
+    .map(repo => repo.latest_version.manifest.source.split('#').shift())
   const list = []
 
   for (const repo of repos) {
